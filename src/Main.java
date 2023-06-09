9d75903fb7abe88566c6b7e676069b287579c5e1
roman <roman.pshenichnikov555@gmail.com>
2023-06-09 20:29:43 +0300

    Задача

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ import java.util.Set;
 public class Main {
     public static void main(String[] args) {
         String[] strings = {"a", "a", "b", "b", "b", "c", "c"};
+
         Set<String> set = new HashSet<String>(Arrays.asList(strings));
+
         String[] resoult = set.toArray(new String[set.size()]);
+
         System.out.println(Arrays.toString(resoult));
     }
 }
